3c2f05fbc7801e569becadd3e35a2a106e00a0fe
Alex Zubkov <zaleksei95@yandex.ru>
2023-10-05 00:07:32 +0300

    V1  ТЗ 13

---
+++ src/main/java/ru/practicum/shareit/item/dao/ItemInMemoryStorageDao.java
@@ -0,0 +1,22 @@
+package ru.practicum.shareit.item.dao;
+
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.User;
+
+import java.util.List;
+
+public interface ItemInMemoryStorageDao {
+    List<Item> getAll();
+
+    Item create(Item item);
+
+    Item update(Item item, Long idItem,Long idOwner);
+
+    void delete(long id);
+
+    Item getItemById(long id);
+    List<Item> getAllItemOwner(Long idOwner);
+
+    List<Item> getItemsBySearch(String text);
+}
