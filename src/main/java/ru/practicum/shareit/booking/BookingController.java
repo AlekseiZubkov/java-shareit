03ab634a29b2a7d943ab68c37cfa6454f1daeb89
Alex Zubkov <zaleksei95@yandex.ru>
2023-10-26 15:03:11 +0300

    ТЗ 14 Проходит преверку Item,User,Booking

--- src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -17,37 +17,37 @@ import java.util.List;
 @AllArgsConstructor
 @RequestMapping(path = "/bookings")
 public class BookingController {
-    private final String booker = "X-Sharer-User-Id";
+
     private final BookingService bookingService;
 
     @PostMapping
-    public BookingDtoOut create(@Valid @RequestBody BookingDto bookingDto, @RequestHeader(booker) Long booker) {
+    public BookingDtoOut create(@Valid @RequestBody BookingDto bookingDto, @RequestHeader("X-Sharer-User-Id") Long booker) {
         log.info("Получен POST-запрос на добавление бронирования владельцем с id={}", booker);
 
         return bookingService.create(bookingDto, booker);
     }
 
     @PatchMapping(value = "/{bookingId}")
-    public BookingDtoOut amend(@RequestHeader(booker) Long userId,
+    public BookingDtoOut amend(@RequestHeader("X-Sharer-User-Id") Long userId,
                                @PathVariable Long bookingId,
                                @RequestParam boolean approved) {
         return bookingService.updateBooking(userId, bookingId, approved);
     }
 
     @GetMapping(value = "/{bookingId}")
-    public BookingDtoOut find(@RequestHeader(booker) Long userId,
+    public BookingDtoOut find(@RequestHeader("X-Sharer-User-Id") Long userId,
                               @PathVariable Long bookingId) {
         return bookingService.findBooking(userId, bookingId);
     }
 
     @GetMapping
-    public List<BookingDtoOut> findAllBookingsByBooker(@RequestHeader(booker) Long userId,
+    public List<BookingDtoOut> findAllBookingsByBooker(@RequestHeader("X-Sharer-User-Id") Long userId,
                                                        @RequestParam(defaultValue = "ALL") String state) {
         return bookingService.findAllBookingsByBooker(userId, state);
     }
 
     @GetMapping(value = "/owner")
-    public List<BookingDtoOut> findAllByOwner(@RequestHeader(booker) Long userId,
+    public List<BookingDtoOut> findAllByOwner(@RequestHeader("X-Sharer-User-Id") Long userId,
                                               @RequestParam(defaultValue = "ALL") String state) {
         return bookingService.findAllBookingsByOwner(userId, state);
     }
