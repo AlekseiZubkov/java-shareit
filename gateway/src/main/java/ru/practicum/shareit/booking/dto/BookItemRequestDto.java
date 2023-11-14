f6eb6530b9311692cbea4949b01769256b797522
Alex Zubkov <zaleksei95@yandex.ru>
2023-11-14 21:44:43 +0300

    Вроде запускается

---
+++ gateway/src/main/java/ru/practicum/shareit/booking/dto/BookItemRequestDto.java
@@ -0,0 +1,25 @@
+package ru.practicum.shareit.booking.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.Future;
+import javax.validation.constraints.FutureOrPresent;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookItemRequestDto {
+
+	private Integer itemId;
+
+	@FutureOrPresent
+	private LocalDateTime start;
+
+	@Future
+	private LocalDateTime end;
+}
