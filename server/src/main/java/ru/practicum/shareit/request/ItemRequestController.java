934535dffa9ba2325b7a136f32f362f5bfd8c50a
Alex Zubkov <zaleksei95@yandex.ru>
2023-11-15 19:11:42 +0300

    16 Тз 1

--- server/src/main/java/ru/practicum/shareit/request/ItemRequestController.java
+++ server/src/main/java/ru/practicum/shareit/request/ItemRequestController.java
@@ -2,12 +2,9 @@ package ru.practicum.shareit.request;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
 
-import javax.validation.Valid;
-import javax.validation.constraints.Min;
 import java.util.List;
 
 /**
@@ -16,7 +13,6 @@ import java.util.List;
 
 @RestController
 @Slf4j
-@Validated
 @RequestMapping(path = "/requests")
 @RequiredArgsConstructor
 public class ItemRequestController {
@@ -25,7 +21,7 @@ public class ItemRequestController {
 
     @PostMapping
     public ItemRequestDto create(@RequestHeader("X-Sharer-User-Id") Long userId,
-                                 @Valid @RequestBody ItemRequestDto itemRequestDto) {
+                                 @RequestBody ItemRequestDto itemRequestDto) {
         return itemRequestService.create(userId, itemRequestDto);
     }
 
@@ -37,8 +33,8 @@ public class ItemRequestController {
     @GetMapping("/all")
     public List<ItemRequestDto> get(
             @RequestHeader("X-Sharer-User-Id") Long userId,
-            @RequestParam(defaultValue = "0") @Min(value = 0) long from,
-            @RequestParam(defaultValue = "10") @Min(value = 0) long size
+            @RequestParam(defaultValue = "0") long from,
+            @RequestParam(defaultValue = "10") long size
     ) {
         return itemRequestService.getAll(userId, from, size);
     }
